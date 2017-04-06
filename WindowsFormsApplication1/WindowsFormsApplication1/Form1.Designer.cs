ce204d8a8503f01ba43f20d790f41846ce45e645
Ira <iralien@airu.com.hk>
2017-04-06 23:48:58 +0800

    新增專案

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ComPort = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.Rate = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Satus = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(29, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SerialPort:";
+            // 
+            // ComPort
+            // 
+            this.ComPort.FormattingEnabled = true;
+            this.ComPort.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7"});
+            this.ComPort.Location = new System.Drawing.Point(129, 51);
+            this.ComPort.Name = "ComPort";
+            this.ComPort.Size = new System.Drawing.Size(121, 20);
+            this.ComPort.TabIndex = 1;
+            this.ComPort.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(330, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BaudRate:";
+            // 
+            // Rate
+            // 
+            this.Rate.FormattingEnabled = true;
+            this.Rate.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.Rate.Location = new System.Drawing.Point(430, 51);
+            this.Rate.Name = "Rate";
+            this.Rate.Size = new System.Drawing.Size(121, 20);
+            this.Rate.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(596, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 145);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(667, 132);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Satus
+            // 
+            this.Satus.AutoSize = true;
+            this.Satus.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Satus.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Satus.Location = new System.Drawing.Point(608, 101);
+            this.Satus.Name = "Satus";
+            this.Satus.Size = new System.Drawing.Size(92, 21);
+            this.Satus.TabIndex = 6;
+            this.Satus.Text = "unconnect";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 339);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(667, 158);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "CardType";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(89, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 650);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Satus);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Rate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ComPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ComPort;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Rate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Satus;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        byte[] Req = { 170, 187, 06, 00, 00, 00, 01, 02, 82, 81 }; //Mifare Request
+        byte[] ReqBuffer = new byte[12];//store Card type
+        byte[] Antcollision = { 170,187,05,00,00,00,02,02,00}; //Mifare Antcollision
+        byte[] AntcollisionBuffer = new byte[14];//store 
+        byte[] DivceBeep = { 170, 187, 06, 00, 00, 00, 06, 01, 100, 99 };//beep
+        byte[] LedColor_RedOn = { 170, 187, 06, 00, 00, 00,07,01,03,05 };      
+        byte[] CardNumber = new byte[4];     
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = ComPort.Text;
+                serialPort1.BaudRate = Int32.Parse(Rate.Text);
+                serialPort1.Open();
+                timer1.Enabled = true;
+                timer1.Interval = 100;
+                Satus.Text = "connect";
+                Satus.ForeColor = Color.Red;
+                button1.Enabled = false;
+                
+            }
+            catch {
+                MessageBox.Show("error");
+            }    
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            serialPort1.Write(Req, 0, Req.Length);
+            Thread.Sleep(200);
+            if (serialPort1.BytesToRead > 0) {
+                
+                textBox1.Text = "";
+                serialPort1.Read(ReqBuffer, 0, ReqBuffer.Length);
+                foreach (byte dd in ReqBuffer) {
+                    textBox1.Text += dd+",";
+                }
+
+                
+            }
+
+            if (ReqBuffer[8] == 0) {
+                
+                textBox2.Text += "    ";
+                serialPort1.Write(Antcollision, 0, Antcollision.Length);
+                Thread.Sleep(300);
+                if (serialPort1.BytesToRead > 0) {
+                    serialPort1.Read(AntcollisionBuffer, 0, AntcollisionBuffer.Length);
+                    if (AntcollisionBuffer[8] == 0) {
+                        if (AntcollisionBuffer[9] == CardNumber[0] &&
+                           AntcollisionBuffer[10] == CardNumber[1] &&
+                           AntcollisionBuffer[11] == CardNumber[2] &&
+                           AntcollisionBuffer[12] == CardNumber[3]
+                            )
+                        {
+                            return;
+                        }
+                        else {
+                            serialPort1.Write(DivceBeep, 0, DivceBeep.Length);
+                            Thread.Sleep(500);
+
+                            serialPort1.Write(LedColor_RedOn, 0, LedColor_RedOn.Length);
+                            Thread.Sleep(600);
+                            
+                            CardNumber[0] = AntcollisionBuffer[9];
+                            CardNumber[1] = AntcollisionBuffer[10];
+                            CardNumber[2] = AntcollisionBuffer[11];
+                            CardNumber[3] = AntcollisionBuffer[12];
+
+                            for (int i = 0; i <= 3; i++) {
+                                textBox2.Text += CardNumber[i]+" ";
+                            }                                             
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
